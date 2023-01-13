a9333f3454883c86535962e805a4def54f90e8a0
187jjay187 <187jjay187@gmail.com>
2023-01-13 13:10:23 +0200

    Add test for home

---
+++ src/__test__/Home.test.js
@@ -0,0 +1,24 @@
+// import
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from '../pages/Navbar';
+import store from '../redux/configureStore';
+import HomePage from '../components/Home';
+
+// test homepage renders
+describe('navbar components render', () => {
+  test('render navbar components', () => {
+    const tree = renderer
+      .create(<Router><Provider store={store}><Navbar /></Provider></Router>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test('render home page', () => {
+    const tree = renderer
+      .create(<Router><Provider store={store}><HomePage /></Provider></Router>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
